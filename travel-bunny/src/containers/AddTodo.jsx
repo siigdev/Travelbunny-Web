b4b0a77a92954b2cb91b244b7d0554358217b853
Sebastian Siig Nørgaard <sesn@netcompany.com>
2020-09-04 16:58:29 +0200

    Working with react-bootstrap

--- travel-bunny/src/containers/AddTodo.jsx
+++ travel-bunny/src/containers/AddTodo.jsx
@@ -1,27 +1,67 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions/todo'
+import { Form, InputGroup, FormControl, Button, Row, Col } from 'react-bootstrap';
 
 const AddTodo = ({ dispatch }) => {
   let input
 
   return (
-    <div>
-      <form onSubmit={e => {
+      <form className="search-box" onSubmit={e => {
         e.preventDefault()
         if (!input.value.trim()) {
           return
         }
         dispatch(addTodo(input.value))
-        input.value = ''
       }}>
-        <input ref={node => input = node} />
-        <button type="submit">
-          Add Todo
-        </button>
+        <Row>
+        <Col>
+        <span>Plan your trip!</span>
+        <Col></Col>
+        <Form.Label column sm={2}>
+          Email
+        </Form.Label>
+        <InputGroup className="mb-3">
+          <FormControl id="location-search" aria-describedby="location-search" />
+          <InputGroup.Append>
+            <InputGroup.Text id="location-search">
+              :)
+            </InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        </Col>
+        <Col>
+        <Col>
+        <label htmlFor="calendar-search">When?</label>
+        </Col>
+        <InputGroup className="mb-3">
+          <FormControl id="calendar-search" aria-describedby="calendar-search" />
+          <InputGroup.Append>
+            <InputGroup.Text id="calendar-search">
+              :)
+            </InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        </Col>
+        <Col>
+        <label htmlFor="location-search">What?</label>
+        <InputGroup className="mb-3">
+          <FormControl id="duration-search" aria-describedby="duration-search" />
+          <InputGroup.Append>
+            <InputGroup.Text id="duration-search">
+              :)
+            </InputGroup.Text>
+          </InputGroup.Append>
+        </InputGroup>
+        </Col>
+        <Col>
+          <Button className="search-trips-button" variant="primary">Primary</Button>
+          </Col>
+          </Row>
       </form>
-    </div>
   )
 }
 
 export default connect()(AddTodo)
+
+
--- travel-bunny/src/containers/App.jsx
+++ travel-bunny/src/containers/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/app.scss';
+import '../styles/main.scss';
 import AddTodo from '../containers/AddTodo'
 import Footer from '../components/global/Footer'
 import VisibleList from '../containers/VisibleList'
