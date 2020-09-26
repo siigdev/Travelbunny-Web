57f4c99dac0590bb94acb9131adedbff67b4673c
siigdev <sebbahlol@gmail.com>
2020-09-26 18:24:02 +0200

    Updates

---
+++ precache-manifest.f05e79307c616bab03f6b134a59dac9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9712cd17bd22d54c49b24b19c85e84b6",
+    "url": "/Travelbunny-Web/index.html"
+  },
+  {
+    "revision": "73be481984feef3167ae",
+    "url": "/Travelbunny-Web/static/css/main.24c097a7.chunk.css"
+  },
+  {
+    "revision": "461373571d51b71d8c15",
+    "url": "/Travelbunny-Web/static/js/2.2155e789.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Travelbunny-Web/static/js/2.2155e789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73be481984feef3167ae",
+    "url": "/Travelbunny-Web/static/js/main.43702e61.chunk.js"
+  },
+  {
+    "revision": "a6415a843c6427b5196b",
+    "url": "/Travelbunny-Web/static/js/runtime-main.67385aae.js"
+  },
+  {
+    "revision": "6c332bfe7e4cacc4e34dd1635c8cb79c",
+    "url": "/Travelbunny-Web/static/media/header-background.6c332bfe.jpg"
+  },
+  {
+    "revision": "72a981e0f37e4477b88cc60209a0c757",
+    "url": "/Travelbunny-Web/static/media/testimage.72a981e0.png"
+  }
+]);
