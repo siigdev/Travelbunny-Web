072da987dd8061ba01c878574cb49958ff7533b3
siigdev <sebbahlol@gmail.com>
2020-10-20 22:39:17 +0200

    Add login modal component

--- src/containers/global/Navigation.jsx
+++ src/containers/global/Navigation.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import LoginModal from '../../containers/modals/LoginModal'
 
-function Navigation() {
-    return (
-        <Navbar variant="light" bg="light" sticky="top">
-            <Navbar.Brand><Link to='/'>Nordico</Link></Navbar.Brand>
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                </Nav>
-                <Nav>
-                    <Link to='/Login' className="nav-link"><li>Login</li></Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+export default class Navigation extends Component {
+    state = {
+        modalOpen: false
+    }
+    handleLoginModal = () => {
+        this.setState((prevState) => {
+           return{
+              modalOpen: !prevState.modalOpen
+           }
+        })
+     }
+
+    render() {
+        return (
+            <Navbar variant="light" bg="light" sticky="top">
+                <Navbar.Brand><Link to='/'>Nordico</Link></Navbar.Brand>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                    </Nav>
+                    <Nav>
+                        <NavItem className="nav-link" onClick={this.handleLoginModal}><li>Login</li></NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+                <LoginModal
+                    modalOpen={this.state.modalOpen}
+                    handleLoginModal={this.handleLoginModal}
+                />
+            </Navbar>
+        )
+    }
 }
-
-export default Navigation;
---
+++ src/containers/modals/LoginModal.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Button, Modal, Form } from 'react-bootstrap';
+
+
+export default class LoginModal extends Component {
+    render() {
+        return (
+            <>
+                <Modal show={this.props.modalOpen} onHide={this.props.handleLoginModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Login</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                    </Form>
+                    </Modal.Body>
+                </Modal>
+            </>
+        );
+    }
+}
+
