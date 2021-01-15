1074f82f47ede89f0a7e47bef6cd31baf6a7b023
siigdev <sebbahlol@gmail.com>
2021-01-15 22:35:42 +0100

    Initial custom theme setup

---
+++ src/config/theme.js
@@ -0,0 +1,59 @@
+const colors = {
+    accent: "#ed5353",
+    primary: "#64baff",
+    secondary: "#8cd5ff",
+    redprimary: "#FF6559",
+    redsecondary: "#FFA8A8",
+    greenprimary: "#50CA58",
+    greensecondary: "#ADE7B1",
+    yellowprimary: "#FFAE59",
+    yellowsecondary: "#FFD5A8",
+    tertiary: "#fff394",
+    black: "#273445",
+    white: "#FFFFFF",
+    gray: "#d4d4d4",
+    gray2: "#abacae",
+    gray3: "#F7F8FA",
+    gray4: "#F9F9FB"
+  };
+  
+  const sizes = {
+    // global sizes
+    base: 16,
+    font: 14,
+    radius: 6,
+    padding: 25,
+  
+    // font sizes
+    h1: 26,
+    h2: 20,
+    h3: 18,
+    title: 18,
+    header: 16,
+    body: 14,
+    caption: 12,
+  };
+  
+  const fonts = {
+    h1: {
+      fontSize: sizes.h1
+    },
+    h2: {
+      fontSize: sizes.h2
+    },
+    h3: {
+      fontSize: sizes.h3
+    },
+    header: {
+      fontSize: sizes.header
+    },
+    title: {
+      fontSize: sizes.title
+    },
+    body: {
+      fontSize: sizes.body
+    },
+    caption: {
+      fontSize: sizes.caption
+    },
+  };
