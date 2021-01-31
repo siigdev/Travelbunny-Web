15a91e3c7fc9b50f15f5d10e6315454af2e8f666
siigdev <sebbahlol@gmail.com>
2021-01-31 15:29:35 +0100

    Use min height main container within app

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -16,31 +16,34 @@ import SiteMap from '../pages/SiteMap'
 import Press from '../pages/Press'
 import Contact from '../pages/Contact'
 import FAQ from '../pages/FAQ'
-import {Switch, Route, HashRouter} from 'react-router-dom';
+import {Switch, Route, HashRouter } from 'react-router-dom';
 import AcceptReserveTripModal from '../containers/modals/AcceptReserveTripModal';
 import ReserveTimedOutModal from '../containers/modals/ReserveTimedOutModal';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <HashRouter basename='/'>
       <div className="App">
         <Navigation />
-        <Switch>
-          <Route path="/" exact component={Main}/>
-          <Route path="/Browse/:start?/:end?/:location?/:length?/:country" component={Browse}/>
-          <Route path="/Trip/:id" component={Trip}/>
-          <Route path="/Purchase/:id" component={Purchase}/>
-          <Route path="/About" component={About}/>
-          <Route path="/Contact" component={Contact}/>
-          <Route path="/Enquiry" component={Enquiry}/>
-          <Route path="/Press" component={Press}/>
-          <Route path="/TermsAndConditions" component={TermsAndConditions}/>
-          <Route path="/Career" component={Career}/>
-          <Route path="/SiteMap" component={SiteMap}/>
-          <Route path="/Privacy" component={Privacy}/>
-          <Route path="/FAQ" component={FAQ}/>
-          <Route component={NotFound}/>
-        </Switch>
+        <Container className="main-container">
+          <Switch>
+            <Route path="/" exact component={Main}/>
+            <Route path="/Browse/:start?/:end?/:location?/:length?/:country" component={Browse}/>
+            <Route path="/Trip/:id" component={Trip}/>
+            <Route path="/Purchase/:id" component={Purchase}/>
+            <Route path="/About" component={About}/>
+            <Route path="/Contact" component={Contact}/>
+            <Route path="/Enquiry" component={Enquiry}/>
+            <Route path="/Press" component={Press}/>
+            <Route path="/TermsAndConditions" component={TermsAndConditions}/>
+            <Route path="/Career" component={Career}/>
+            <Route path="/SiteMap" component={SiteMap}/>
+            <Route path="/Privacy" component={Privacy}/>
+            <Route path="/FAQ" component={FAQ}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </Container>
         <AcceptReserveTripModal />
         <ReserveTimedOutModal />
         <Footer />
