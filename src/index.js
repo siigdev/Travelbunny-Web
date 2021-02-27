089b4a91b0649cc6cecc91ffc7af09a2c255329f
The Nordico Team <77929642+Nordico-team@users.noreply.github.com>
2021-02-27 10:18:29 +0000

    Update index.js to adapt to ripple
    Updated to adapt index.js to ripple effect
    More info at https://github.com/siigdev/Travelbunny-Web/pull/14 or at my doc https://docs.google.com/document/d/1Ag1k_gKMHgfpTw1Sk4QUn-z8uoQgB0zX6TCK5yz5I4c/edit?usp=sharing.

--- src/index.js
+++ src/index.js
@@ -17,3 +17,38 @@ const store = createStore(
 
 ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById('root'));
 serviceWorker.register();
+
+
+// Here goes the ripple code
+// by Tiago Rangel, @tiagorangel2011
+
+const Button = ({ children }) => {
+  function createRipple(event) {
+    const button = event.currentTarget;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
+  }
+
+  return <button onClick={createRipple}>{children}</button>;
+};
+
+const App = () => {
+  return <Button>Click Me</Button>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
