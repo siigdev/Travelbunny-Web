1a94aa7f0c25f6a5a9ed3753bc73ec484e5e21df
Sebastian Siig Nørgaard <sesn@netcompany.com>
2021-05-05 20:34:45 +0200

    Added functionality for the settings search box

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,50 +1,71 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import Loading from '../components/global/Loading'
 import LoadingTripGeneration from '../components/global/LoadingTripGeneration/LoadingTripGeneration'
 import TripBox from '../components/browse_page/TripBox'
 import { Container, Row, Col, Button, Form, InputGroup, FormControl, Alert } from 'react-bootstrap';
 import { saveTripsToState } from '../actions/tripActions/tripActions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendar, faSortAmountUp, faSortAmountDown, faSortNumericUp, faSearch, faDollarSign, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar, faCalendarCheck, faSortAmountUp, faSortAmountDown, faSortNumericUp, faDollarSign, faGlobe, faPlaneDeparture } from "@fortawesome/free-solid-svg-icons";
 import cities from '../constants/cities';
 import Select, { components } from 'react-select';
+import DatePicker from 'react-datepicker';
+import { saveSearchoptions } from '../actions/tripActions/tripActions'
+import * as timeHelper from '../helpers/timeHelper'
+import { Typeahead } from 'react-bootstrap-typeahead';
+
+const citiesList = cities.map(x => x.value);
 
 class Browse extends Component {
     constructor(props) {
         super(props);
         this.match = props.match.params;
+        this.props.saveSearchoptions(this.match.length, this.match.location, this.match.start, this.match.end);
         this.state = {
             error: '',
-            loading: true,
-            price: 975
+            loading: false,
+            loadingWithSearchParams: false,
+            price: 975,
+            start: this.match.start,
+            end: this.match.end,
+            location: this.match.location,
+            country: this.match.country,
+            length: this.match.length
         }
     }
     componentDidMount() {
-        var params = new URLSearchParams({
-            windowStart: this.match.start,
-            windowEnd: this.match.end,
-            initialLocation: this.match.location,
-            stayLength: this.match.length,
-            country: this.match.country,
-            currency: 'EUR',
-            locale: 'en-US'
-        })
-
         if(this.props.trips !== null && this.props.trips !== undefined && this.props.location.new !== true) {
             this.setState({ loading: false })
         }
-        else {
-            fetch("https://hj240syse0.execute-api.eu-central-1.amazonaws.com/prod/?" + params)
-                .then(response => response.json())
-                .then(response => {
-                    this.props.saveTripsToState(response.trips)
-                    this.setState({ loading: false })
-                })
-                .catch((error) => {
-                    this.setState({ loading: false, error: error })
-                });
-            }
-        }
+        this.saveStateToParamsAndSearchTrip();
+    }
+
+    saveStateToParamsAndSearchTrip(){
+        this.setState({loadingWithSearchParams: true}) 
+        var params = (new URLSearchParams({
+            windowStart: (typeof(this.state.start) === 'object') ? timeHelper.converDateToYYYYMMDD(this.state.start) : this.state.start,
+            windowEnd: (typeof(this.state.end) === 'object') ? timeHelper.converDateToYYYYMMDD(this.state.end) :this.state.end,
+            initialLocation: this.state.location,
+            stayLength: this.state.length,
+            country: this.state.country,
+            currency: 'EUR',
+            locale: 'en-US'
+        }));
+        this.searchTrip(params)
+    }
+
+    searchTrip(params) {
+        fetch("https://hj240syse0.execute-api.eu-central-1.amazonaws.com/prod/?" + params)
+            .then(response => response.json())
+            .then(response => {
+                this.props.saveTripsToState(response.trips)
+                this.setState({ loading: false, loadingWithSearchParams: false })
+            })
+            .catch((error) => {
+                this.setState({ loading: false, loadingWithSearchParams: false, error: error })
+            });
+    }
+
     viewTrip(trip) {
         this.props.history.push({
             pathname: '/Trip/' + trip.id,
@@ -83,14 +104,22 @@ class Browse extends Component {
                             <Button variant="link"><FontAwesomeIcon icon={faSortAmountDown} size="1x" /> Price High to Low</Button>
                             <Button variant="link"><FontAwesomeIcon icon={faSortNumericUp} size="1x" /> Number of Cities</Button>
                         </Container>
-                        {this.renderTrips()}
+                        {this.state.loadingWithSearchParams ? <Loading /> : this.renderTrips()}
+                        
                     </Col>
 
                     <Col className="search-settings">
                         <InputGroup className="mb-2">
-                            <FormControl id="search-trip-id" placeholder="Trip ID" />
+                        <Typeahead
+                                id="locationpicker"
+                                defaultSelected={citiesList.slice(citiesList.indexOf(this.state.location), citiesList.indexOf(this.state.location) + 1)}
+                                className="locationpicker-form"
+                                onChange={(event) => this.setState({ location: event[0] })}
+                                options={citiesList}
+                                placeholder="Select your location"
+                                />
                             <InputGroup.Append>
-                                <InputGroup.Text><FontAwesomeIcon icon={faSearch} size="1x" /></InputGroup.Text>
+                                <InputGroup.Text><FontAwesomeIcon icon={faPlaneDeparture} size="1x"/></InputGroup.Text>
                             </InputGroup.Append>
                         </InputGroup>
                         <InputGroup className="mb-2">
@@ -99,7 +128,7 @@ class Browse extends Component {
                             placeholder={'Destinations'}
                             isMulti
                             name="destinations"
-                            id="search-destination"
+                            id="search-must-go-destination"
                             options={cities}
                             components={{ DropdownIndicator }}
                             className="basic-multi-select"
@@ -108,9 +137,31 @@ class Browse extends Component {
                         />
                         </InputGroup>
                         <InputGroup className="mb-2">
-                            <FormControl id="search-date" placeholder="Date" />
+                        <DatePicker
+                                    dateFormat="yyyy-MM-dd"
+                                    className="form-control datepicker-form"
+                                    id="search-startdate"
+                                    selected={new Date(this.state.start)}
+                                    onChange={date => this.setState({ start: date })}
+                                    placeholderText={this.state.start}
+                                    minDate={new Date()}
+                                />
                             <InputGroup.Append>
                                 <InputGroup.Text><FontAwesomeIcon icon={faCalendar} size="1x" /></InputGroup.Text>
+                            </InputGroup.Append>
+                        </InputGroup>
+                        <InputGroup className="mb-2">
+                        <DatePicker
+                                    dateFormat="yyyy-MM-dd"
+                                    className="form-control datepicker-form"
+                                    id="search-endDate"
+                                    selected={new Date(this.state.end)}
+                                    onChange={date => this.setState({ end: date })}
+                                    placeholderText={this.state.end}
+                                    minDate={new Date()}
+                                />
+                            <InputGroup.Append>
+                                <InputGroup.Text><FontAwesomeIcon icon={faCalendarCheck} size="1x" /></InputGroup.Text>
                             </InputGroup.Append>
                         </InputGroup>
 
@@ -132,7 +183,7 @@ class Browse extends Component {
                         <Form.Check type="checkbox" label="Attractions" />
                         <Form.Check type="checkbox" label="Luxury" />
                         <Form.Check type="checkbox" label="Romantic" />
-                        <Button variant="secondary">Apply settings</Button>
+                        <Button variant="secondary" onClick={this.saveStateToParamsAndSearchTrip.bind(this)}>Search</Button>
                     </Col>
                 </Row>
             </Container>
@@ -141,7 +192,7 @@ class Browse extends Component {
 }
 const CaretDownIcon = () => {
     return <FontAwesomeIcon icon={faGlobe} size="1x" className="bootstrap-grey" style={{marginLeft: "3px", marginRight: "3px"}}/>;
-  };
+};
 const DropdownIndicator = props => {
     return (
       <components.DropdownIndicator {...props} >
@@ -149,15 +200,16 @@ const DropdownIndicator = props => {
       </components.DropdownIndicator>
     );
   };
-
 const mapStateToProps = (state) => {
     return {
-        trips: state.trip.trips
+        trips: state.trip.trips,
+        searchOptions: state.trip.searchOptions
     }
 }
 const mapDispatchToProps = (dispatch) => {
     return {
-        saveTripsToState: (trips) => dispatch(saveTripsToState(trips))
+        saveTripsToState: (trips) => dispatch(saveTripsToState(trips)),
+        saveSearchoptions: (tripLengthState, departureState, startDate, endDate) => dispatch(saveSearchoptions(tripLengthState, departureState, startDate, endDate))
     }
 }
 
